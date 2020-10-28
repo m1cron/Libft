bd8b50111b1bcf610a3dc201f5f7eb6a43cf0755
Clinton Sibyl <csibyl@am-a3.msk.21-school.ru>
2020-10-28 17:29:04 +0300

    added ft_memcmp.c

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@ void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 
 
 #endif
