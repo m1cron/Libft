a90a1d591f6bb7ca89822270ae67e95e82f07f39
note-pc <m1cron4900@gmail.com>
2020-10-28 13:27:39 +0300

    added ft_memset.c, libft.h, Cmake file

---
+++ ft_memset.c
@@ -0,0 +1,17 @@
+//
+// Created by micron on 28.10.2020.
+//
+
+#include "libft.h"
+
+void    *ft_memset(void *s, int c, size_t n)
+{
+    unsigned char *dest;
+    unsigned char ch;
+
+    dest = (unsigned char *)s;
+    ch = (unsigned char)c;
+    while (len--)
+        *dest++ = ch;
+    return (s);
+}
---
+++ libft.h
@@ -0,0 +1,10 @@
+//
+// Created by micron on 28.10.2020.
+//
+
+#ifndef LIBFT_LIBFT_H
+#define LIBFT_LIBFT_H
+
+
+
+#endif //LIBFT_LIBFT_H
