0777b228a3f53962369a5caa041a9823f13c93e3
note-pc <m1cron4900@gmail.com>
2020-10-28 16:36:50 +0300

    TODO ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,18 @@
+//
+// Created by micron on 28.10.2020.
+//
+
+#include "libft.h"
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *d;
+    const unsigned char *s;
+
+    d = (unsigned char *)dest;
+    s = (unsigned char *)src;
+    if (d > s)
+        
+
+    return (dest);
+}
